0af093b1adab3002f29d2ac09d11b05b1c62c0df
Cristian Arevalo <csarevalo1@gmail.com>
2024-10-28 20:26:46 -0700

    created network helper and refactored location services

--- lib/src/screens/home_screen_content.dart
+++ lib/src/screens/home_screen_content.dart
@@ -1,4 +1,5 @@
-import 'package:clima/src/utils/location.dart';
+import 'package:clima/src/services/location.dart';
+import 'package:clima/src/services/networking.dart';
 import 'package:clima/src/widgets/weather_card.dart';
 import 'package:clima/src/widgets/weather_message_card.dart';
 import 'package:flutter/material.dart';
@@ -17,28 +18,48 @@ class HomeScreenContent extends StatefulWidget {
 class _HomeScreenContentState extends State<HomeScreenContent> {
   final Location location = Location();
   late Position? position;
+  dynamic weatherData;
 
   @override
   void initState() {
     super.initState();
     getPosition();
+    // getWeatherData();
   }
 
-  void getPosition() async {
+  Future<void> getPosition() async {
     position = await location.determinePosition();
+  }
+
+  Future<void> getWeatherData() async {
+    await getPosition();
     double lat = position?.latitude ?? 0;
     double long = position?.longitude ?? 0;
     debugPrint('lat: $lat, long: $long');
+
+    const String openWeatherUrl = 'https://api.openweathermap.org/data';
+    const String apiKey = 'f6a2b2f36fe1df4ac64e75e29ad0f49e';
+
+    NetworkHelper networkHelper = NetworkHelper(
+      // '$openWeatherUrl/3.0/onecall?lat=$lat&lon=$long&exclude=hourly,daily&appid=$apiKey',
+      '$openWeatherUrl/2.5/weather?lat=$lat&lon=$long&APPID=$apiKey',
+    );
+    weatherData = await networkHelper.getWeatherData();
+
+    return;
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          WeatherCard(),
+          WeatherCard(
+            openWeatherIconString: weatherData?['weather'][0]['icon'] ?? '02n',
+            temperature: weatherData?['main']['temp'] ?? 71,
+          ),
           WeatherMessageCard(),
         ],
       ),
--- lib/src/widgets/weather_card.dart
+++ lib/src/widgets/weather_card.dart
@@ -5,22 +5,33 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class WeatherCard extends StatelessWidget {
   const WeatherCard({
     super.key,
+    this.openWeatherIconString = '',
+    required this.temperature,
   });
+  final String openWeatherIconString;
+  final double temperature;
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(25.0),
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
         children: [
-          FaIcon(
-            FontAwesomeIcons.solidSun,
-            size: 70,
+          Row(
+            children: [
+              const FaIcon(
+                FontAwesomeIcons.solidSun,
+                size: 70,
+              ),
+              Image.network(
+                'http://openweathermap.org/img/wn/$openWeatherIconString@2x.png',
+              )
+            ],
           ),
-          TempCard(temperature: 75),
+          TempCard(temperature: temperature),
         ],
       ),
     );
