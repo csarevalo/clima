ce0dd5c1bf2d2206ad18453686f5a827428a2918
Cristian Arevalo <csarevalo1@gmail.com>
2024-10-28 21:54:27 -0700

    remove unnecessary comment

--- lib/src/services/networking.dart
+++ lib/src/services/networking.dart
@@ -18,25 +18,3 @@ class NetworkHelper {
     return null;
   }
 }
-
-// Future<void> getWeatherData() async {
-//   await getPosition();
-//   double lat = position?.latitude ?? 0;
-//   double long = position?.longitude ?? 0;
-//   debugPrint('lat: $lat, long: $long');
-
-//   const String openWeatherUrl = 'https://api.openweathermap.org/data';
-//   const String apiKey = 'f6a2b2f36fe1df4ac64e75e29ad0f49e';
-
-//   http.Response response = await http.get(Uri.parse(
-//     // '$openWeatherUrl/3.0/onecall?lat=$lat&lon=$long&exclude=hourly,daily&appid=$apiKey',
-//     '$openWeatherUrl/2.5/weather?lat=$lat&lon=$long&APPID=$apiKey',
-//   ));
-//   debugPrint('Response status: ${response.statusCode}');
-//   if (response.statusCode == 200) {
-//     debugPrint('Response body: ${response.body}');
-//     weatherData = jsonDecode(response.body);
-//   }
-
-//   return;
-// }
