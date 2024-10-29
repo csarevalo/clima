3eee39b33535932b6cc7bccf2aadb9141a859b7e
Cristian Arevalo <csarevalo1@gmail.com>
2024-10-28 21:12:57 -0700

    removed getWeatherData from homeScreen and updated weather provider

--- lib/src/screens/home_screen_content.dart
+++ lib/src/screens/home_screen_content.dart
@@ -1,10 +1,8 @@
 import 'package:clima/src/services/location.dart';
-import 'package:clima/src/services/networking.dart';
 import 'package:clima/src/widgets/weather_card.dart';
 import 'package:clima/src/widgets/weather_message_card.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
 
 class HomeScreenContent extends StatefulWidget {
   const HomeScreenContent({
@@ -24,30 +22,12 @@ class _HomeScreenContentState extends State<HomeScreenContent> {
   void initState() {
     super.initState();
     getPosition();
-    // getWeatherData();
   }
 
   Future<void> getPosition() async {
     position = await location.getPosition();
   }
 
-  Future<void> getWeatherData() async {
-    await getPosition();
-    double lat = position?.latitude ?? 0;
-    double long = position?.longitude ?? 0;
-    debugPrint('lat: $lat, long: $long');
-
-    const String openWeatherUrl = 'https://api.openweathermap.org/data';
-    const String apiKey = 'f6a2b2f36fe1df4ac64e75e29ad0f49e';
-
-    NetworkHelper networkHelper = NetworkHelper(
-      // '$openWeatherUrl/3.0/onecall?lat=$lat&lon=$long&exclude=hourly,daily&appid=$apiKey',
-      '$openWeatherUrl/2.5/weather?lat=$lat&lon=$long&APPID=$apiKey',
-    );
-    weatherData = await networkHelper.getWeatherData();
-
-    return;
-  }
 
   @override
   Widget build(BuildContext context) {
