484199e8481e1a2c0297b71344795140fe9713e6
Cristian Arevalo <csarevalo1@gmail.com>
2024-10-29 23:07:43 -0700

    improved loading screen

--- lib/src/screens/loading_screen.dart
+++ lib/src/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatelessWidget {
   const LoadingScreen({
@@ -9,16 +10,21 @@ class LoadingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Container(
-      color: Colors.blueGrey,
+      color: Colors.blue[900],
       height: double.infinity,
       width: double.infinity,
       child: const Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SizedBox.square(
-            dimension: 100,
-            child: CircularProgressIndicator(),
-          )
+          // SizedBox.square(
+          //   dimension: 100,
+          //   child: CircularProgressIndicator(),
+          // ),
+          SpinKitSpinningLines(
+            size: 100,
+            lineWidth: 5.0,
+            color: Colors.red,
+          ),
         ],
       ),
     ));
