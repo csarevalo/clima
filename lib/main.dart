9bee55c5671b5a4ca9a7da3ed4748253a7325522
Cristian Arevalo <csarevalo1@gmail.com>
2024-10-29 22:01:28 -0700

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,17 @@ import 'package:clima/src/clima_app.dart';
 
 import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
+  // WidgetsBinding widgetsBinding =
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(
     const ClimaApp(),
   );
+  // whenever your initialization is completed, remove the splash screen:
+  // FlutterNativeSplash.remove();
 }
